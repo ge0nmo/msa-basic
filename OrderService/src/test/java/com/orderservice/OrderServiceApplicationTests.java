91efec5fa7d032d54449cb79bc3d3c194e95c427
ge0nmo <ands0927@naver.com>
2023-07-06 00:43:35 +0900

    feat/order service

---
+++ OrderService/src/test/java/com/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
