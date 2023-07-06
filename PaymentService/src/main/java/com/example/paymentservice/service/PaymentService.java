4618f59b6f59e2be0840b916f870e1dbea73bba4
ge0nmo <ands0927@naver.com>
2023-07-07 00:13:34 +0900

    feat/payment - eureka client

--- PaymentService/src/main/java/com/example/paymentservice/service/PaymentService.java
+++ PaymentService/src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -4,6 +4,7 @@ import com.example.paymentservice.entity.Payment;
 import com.example.paymentservice.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
